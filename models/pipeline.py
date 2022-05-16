bf2f608eedfbd76c1b1354c00a11b3f06c0881ae
Dali <2110837882@stud.fh-kufstein.ac.at>
2022-05-16 23:25:06 +0000

    pipeline updated

--- models/pipeline.py
+++ models/pipeline.py
@@ -3,44 +3,17 @@ import pandas as pd
 import numpy as np
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler,OrdinalEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
 
 
 data = pd.read_csv('./data/raw/train_FD001.txt', sep=' ', header=None)
-
-
-class FeatureSelector:
-    '''This class selects the categorical features or the numerical features of the data set. 
-
-    Prameters
-    ----------
-    use_numbers: bool (default: True)
-        select True if numerical features, or False if categorical features
-    '''
-    def __init__(self, use_numbers=True):
-        assert type(use_numbers)==bool, "use_numbers must be of type bool"
-        self.use_numbers = use_numbers
-    
-    def fit(self, X, y=None):
-        return self
-    
-    def transform(self, X, y=None):
-        assert type(X)==pd.DataFrame, "X must be of type pandas.DataFrame"
-        if self.use_numbers:
-            return X.select_dtypes(include="number")
-        else:
-            return X.select_dtypes(exclude="number")
-    
-    def fit_transform(self, X, y=None):
-        self.fit(X, y)
-        return self.transform(X, y)
 
 class RemoveColumn:
     ''' This class evaluates all columns separately if the whole column consists missing values and removes the whole column.'''
 
     def __init__(self):
-        #assert type(self)==pd.DataFrame, "DataFrame must be of type pandas.DataFrame"
-        #self.use_numbers = use_numbers
         return None
     
     def fit(self, X, y=None):
@@ -48,8 +21,9 @@ class RemoveColumn:
     
     def transform(self, X, y=None):
         assert type(X)==pd.DataFrame, "X must be of type pandas.DataFrame"
-        for i in range(0,len(X.columns)):
-            if X.iloc[:,i].isna().unique() == 'True':
+        
+        for i in X.columns:          
+            if X[i].isna().all() == True:
                 X.drop(i,axis=1, inplace=True)  # delete whole column
         return X
     
@@ -57,33 +31,67 @@ class RemoveColumn:
         self.fit(X, y)
         return self.transform(X, y)
 
+# Define column headers, as the data should have column names
+preamble_headers = [
+    "unit_number",
+    "cycles",
+    "operational_setting_1",
+    "operational_setting_2",
+    "operational_setting_3"
+]
+sensor_headers = [f"sensor_{x}" for x in range(1, 24)]
+
+# Combine column headers
+column_headers = preamble_headers + sensor_headers
+all_headers = column_headers + ["rul"]
+
+# Assign the headers
+data.columns = column_headers
+
+
+# user output variable
+SEL_OUTPUT = data.columns[8]  # user can put the selected feature as output variable
 
 # Separate in X and y
-data.drop(np.arange(5), axis=1, inplace=True) # drop first 5 features, as they are categorical but stored as numerical
-SEL_OUTPUT = 5  # user can put the selected index, which feature should be used as output variable
 X = data.drop(SEL_OUTPUT, axis=1)
-y = data.iloc[:,SEL_OUTPUT]
+y = data[SEL_OUTPUT]
+
+# user input variables
+SEL_CAT_FEATURES = X.columns[:5].values # user can put the selected categorical features 
+SEL_NUM_FEATURES = X.columns[5:].values # user can put the selected numerical features 
+ALL_FEATURES = X.columns.values
+
+
 
 cat_pipe = Pipeline(steps=[
-    ("selector", FeatureSelector(use_numbers=False)),
     ('remove', RemoveColumn()),
-    ("impute", SimpleImputer(strategy="constant", fill_value="unknown")),
+    ('impute_cat', SimpleImputer(strategy='constant', fill_value=999)),
+    ('encoder_cat', OrdinalEncoder()) # does not work well? 
+    
 ])
 
 num_pipe = Pipeline(steps=[
-    ("selector", FeatureSelector(use_numbers=True)),
     ('remove', RemoveColumn()),
-    ("impute", SimpleImputer(strategy="constant", fill_value=0)),
-    ("scaling", MinMaxScaler())
+    ('impute_num', SimpleImputer(strategy='constant', fill_value=0)),
+    ('scaling', MinMaxScaler())
 ])
 
-union = FeatureUnion(transformer_list=[
-    ("num_pipe", num_pipe),
-    ("non_num_pipe", cat_pipe)
-])
-
-
-union.fit_transform(X)
 
 # questions:
 # what to do with the cycle time? 
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", num_pipe, SEL_NUM_FEATURES),
+        ("cat", cat_pipe, SEL_CAT_FEATURES)
+    ]
+)
+
+test = preprocessor.fit_transform(X)
+
+o = pd.DataFrame(test,columns=X.columns[:25])
+print(o)
+
+r=OrdinalEncoder()
+check = r.fit_transform(X[SEL_CAT_FEATURES].values)
+print(check)
