9fbc159035128bf421d5ad49c46e6ecd35912590
Niklas Mayr <2110837326@stud.fh-kufstein.ac.at>
2022-06-04 11:08:49 +0000

    created frontend in streamlit

--- ui.py
+++ ui.py
@@ -7,63 +7,48 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
+import time
+import src
+
+# import pycaret output
+
+df = pd.read_feather('data/final/results.feather')
+
 st.header('Get Predictions out of your Data')
 
-img = Image.open("pics/nodes.jpg")
-st.image(img, width=750)
+# sidebar
 
-# Selection box
-
-# first argument takes the title of the selectionbox
-# second argument takes options
 data = st.sidebar.selectbox("Dataset: ",
 					['Turbofan'])
 
-# print the selected hobby
-st.write("Your dataset is: ", data)
-
-# radio button
-# first argument is the title of the radio button
-# second argument is the options for the ratio button
+# task definition
 task = st.sidebar.radio("Task: ", ('Regression', 'Classification'))
 
-# conditional statement to print
-# Male if male is selected else print female
-# show the result using the success function
 if (task == 'Regression'):
     st.sidebar.success("Regression")
 else:
     st.sidebar.success("Classification")
 
+pred = st.sidebar.button("Latest training results!")
 
-# Create a simple button that does nothing
-st.sidebar.button("Get Prediction")
-
-# Create a button, that when clicked, shows a text
-if(st.sidebar.button("About")):
-	st.sidebar.text("WIP")
+rtrain = st.sidebar.button("Retrain Dataset")
 
 
+img = Image.open("pics/nodes.jpg")
+st.image(img, width=750)
 
+st.write("Your dataset is: ", data)
 
+# Loading spinner
 
+if pred:
+    with st.spinner('Predicting...'):
+        time.sleep(5)
+st.success('Done!')
 
+if pred:
+    st.dataframe(df)
 
-# DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+if rtrain:
+    src.run()
 
-
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase, axis='columns', inplace=True)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     return data
-
-# Create a text element and let the reader know the data is loading.
-# data_load_state = st.text('Loading data...')
-# # Load 10,000 rows of data into the dataframe.
-# data = load_data(10000)
-# # Notify the reader that the data was successfully loaded.
-# data_load_state.text('Loading data...done!')
