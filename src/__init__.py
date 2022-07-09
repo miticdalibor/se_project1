1bd5b0c9985712c83fee5d17d4dbbac32e53423e
Dali <2110837882@stud.fh-kufstein.ac.at>
2022-07-09 17:10:55 +0000

    fix: hashing st.cache

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ import src.process as process
 import src.model as model
 import streamlit as st
 
-@st.cache
+@st.cache(hash_funcs={"MyUnhashableClass": lambda _: None})
 def run():
     
     process.run()
